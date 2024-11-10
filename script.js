57a6293cb642731913989c749aa2686ba3ac0cd5
ilyan <ilyanbendib@gmail.com>
2024-11-10 21:05:54 +0100

    modif projet front

--- script.js
+++ script.js
@@ -72,109 +72,107 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // Fonction pour afficher une description et cacher les autres
-    function showDescription(descriptionId) {
-        // Masquer la section des projets
-        document.getElementById('ecole42').style.display = 'none';
+    // function showDescription(descriptionId) {
+    //     // Masquer la section des projets
+    //     document.getElementById('ecole42').style.display = 'none';
 
-        // Masquer toutes les descriptions avant d'en afficher une
-        document.querySelectorAll('.description').forEach(function(desc) {
-            desc.classList.remove('show');
-            desc.style.display = 'none';
-        });
+    //     // Masquer toutes les descriptions avant d'en afficher une
+    //     document.querySelectorAll('.description').forEach(function(desc) {
+    //         desc.classList.remove('show');
+    //         desc.style.display = 'none';
+    //     });
 
-        // Afficher la description demandée
-        var description = document.getElementById(descriptionId);
-        var button = document.querySelector('.back-button');
-        if (description) {
-            description.style.display = 'block'; // Assurer que la section est visible
-            setTimeout(function() {
-                description.classList.add('show'); // Ajouter la classe pour afficher avec transition
-            }, 10); // Légère pause pour permettre à la section d'être affichée avant de commencer la transition
+    //     // Afficher la description demandée
+    //     var description = document.getElementById(descriptionId);
+    //     var button = document.querySelector('.back-button');
+    //     if (description) {
+    //         description.style.display = 'block'; // Assurer que la section est visible
+    //         setTimeout(function() {
+    //             description.classList.add('show'); // Ajouter la classe pour afficher avec transition
+    //         }, 10); // Légère pause pour permettre à la section d'être affichée avant de commencer la transition
 
-            button.style.display = 'block'; // Afficher le bouton de retour
-        }
-    }
+    //         button.style.display = 'block'; // Afficher le bouton de retour
+    //     }
+    // }
 
-    // Fonction pour cacher les descriptions et réafficher les projets
-    function hideDescription() {
-        var video = document.getElementById('video_in_description');
-        if (video) {
-            video.pause(); // Mettre la vidéo en pause avant de masquer la description
-        }
+    // // Fonction pour cacher les descriptions et réafficher les projets
+    // function hideDescription() {
+    //     var video = document.getElementById('video_in_description');
+    //     if (video) {
+    //         video.pause(); // Mettre la vidéo en pause avant de masquer la description
+    //     }
 
-        document.querySelectorAll('.description').forEach(function(description) {
-            if (description.classList.contains('show')) {
-                description.classList.remove('show'); // Retirer la classe pour cacher avec transition
-                setTimeout(function() {
-                    description.style.display = 'none'; // Assurer que la section est bien cachée
-                }, 1000); // Correspond à la durée de la transition
-            }
-        });
+    //     document.querySelectorAll('.description').forEach(function(description) {
+    //         if (description.classList.contains('show')) {
+    //             description.classList.remove('show'); // Retirer la classe pour cacher avec transition
+    //             setTimeout(function() {
+    //                 description.style.display = 'none'; // Assurer que la section est bien cachée
+    //             }, 1000); // Correspond à la durée de la transition
+    //         }
+    //     });
 
-        // Réafficher la section des projets
-        setTimeout(function() {
-            var ecole42 = document.getElementById('ecole42');
-            ecole42.style.display = 'block';
-            ecole42.style.opacity = 0;
-            setTimeout(function() {
-                ecole42.style.transition = 'opacity 1s ease-in-out';
-                ecole42.style.opacity = 1;
-            }, 50);
-        }, 1000); // Délai correspondant à la durée de la transition de fade-out
-    }
+    //     // Réafficher la section des projets
+    //     setTimeout(function() {
+    //         var ecole42 = document.getElementById('ecole42');
+    //         ecole42.style.display = 'block';
+    //         ecole42.style.opacity = 0;
+    //         setTimeout(function() {
+    //             ecole42.style.transition = 'opacity 1s ease-in-out';
+    //             ecole42.style.opacity = 1;
+    //         }, 50);
+    //     }, 1000); // Délai correspondant à la durée de la transition de fade-out
+    // }
 
-    // Ajout des gestionnaires d'événements pour chaque projet
-    document.getElementById('projet_minishell').addEventListener('click', function(event) {
-        showDescription('description_minishell');
-    });
+    // document.getElementById('projet_minishell').addEventListener('click', function(event) {
+    //     showDescription('description_minishell');
+    // });
 
-    document.getElementById('projet_cub3d').addEventListener('click', function(event) {
-        showDescription('description_cub3d');
-    });
+    // document.getElementById('projet_cub3d').addEventListener('click', function(event) {
+    //     showDescription('description_cub3d');
+    // });
 
-    document.getElementById('projet_Solong').addEventListener('click', function(event) {
-        showDescription('description_Solong');
-    });
+    // document.getElementById('projet_Solong').addEventListener('click', function(event) {
+    //     showDescription('description_Solong');
+    // });
 
-    document.getElementById('projet_pushswap').addEventListener('click', function(event) {
-        showDescription('description_pushswap');
-    });
+    // document.getElementById('projet_pushswap').addEventListener('click', function(event) {
+    //     showDescription('description_pushswap');
+    // });
 
-    document.getElementById('projet_philo').addEventListener('click', function(event) {
-        showDescription('description_philo');
-    });
+    // document.getElementById('projet_philo').addEventListener('click', function(event) {
+    //     showDescription('description_philo');
+    // });
 
-    document.getElementById('projet_minitalk').addEventListener('click', function(event) {
-        showDescription('description_minitalk');
-    });
+    // document.getElementById('projet_minitalk').addEventListener('click', function(event) {
+    //     showDescription('description_minitalk');
+    // });
 
-    // Gestion du bouton de retour
-    document.querySelectorAll('.back-button').forEach(function(button) {
-        button.addEventListener('click', function(event) {
-            event.preventDefault();
-            hideDescription();
-        });
-    });
+    // document.querySelectorAll('.back-button').forEach(function(button) {
+    //     button.addEventListener('click', function(event) {
+    //         event.preventDefault();
+    //         hideDescription();
+    //     });
+    // });
 
     // Fonction pour ajuster le comportement de défilement
-    function adjustScrollBehavior() {
-        var container = document.getElementById('ecole42-container');
+    // function adjustScrollBehavior() {
+    //     var container = document.getElementById('ecole42-container');
 
-        // Vérifie si le contenu dépasse la hauteur visible
-        if (container.scrollHeight > container.clientHeight) {
-            // Si le contenu dépasse, permet le défilement
-            container.style.overflow = 'auto';
-        } else {
-            // Sinon, cache les barres de défilement
-            container.style.overflow = 'hidden';
-        }
-    }
+    //     // Vérifie si le contenu dépasse la hauteur visible
+    //     if (container.scrollHeight > container.clientHeight) {
+    //         // Si le contenu dépasse, permet le défilement
+    //         container.style.overflow = 'auto';
+    //     } else {
+    //         // Sinon, cache les barres de défilement
+    //         container.style.overflow = 'hidden';
+    //     }
+    // }
 
-    // Appel de la fonction lors du chargement de la page
-    window.onload = adjustScrollBehavior;
+    // // Appel de la fonction lors du chargement de la page
+    // window.onload = adjustScrollBehavior;
 
-    // Appel de la fonction lors du redimensionnement de la fenêtre
-    window.onresize = adjustScrollBehavior;
+    // // Appel de la fonction lors du redimensionnement de la fenêtre
+    // window.onresize = adjustScrollBehavior;
 
     document.addEventListener('mousemove', function(e) {
         const light = document.getElementById('cursor-light');
@@ -203,6 +201,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	navvLinks.forEach(link => {
 		link.addEventListener('click', closePanel);
 	});
+
 
 
 });
