81ddbf7972d51715a199e7a7c7f3b426acdb5cbf
ilyan <ilyanbendib@gmail.com>
2024-11-18 23:14:51 +0100

    fin langue system

--- script.js
+++ script.js
@@ -232,6 +232,17 @@ document.addEventListener("DOMContentLoaded", function() {
         'Etudiant a 42 lyon': 'Étudiant à 42 Lyon',
         "En tant qu'étudiant à l'école 42, je me consacre à l'apprentissage continu en informatique. Je développe mes compétences en programmation et en résolution de problèmes techniques au sein de cette école innovante.": "En tant qu'étudiant à l'école 42, je me consacre à l'apprentissage continu en informatique. Je développe mes compétences en programmation et en résolution de problèmes techniques au sein de cette école innovante. ",
         'développeur de jeux vidéo': 'Développeur de jeux vidéo',
+		"En tant que passionné de jeux vidéo, je crée et développe mes propres jeux en utilisant des outils comme Unity. Je me concentre sur la réalisation de projets personnels qui me permettent d'explorer ma créativité et mes compétences techniques. " : "En tant que passionné de jeux vidéo, je crée et développe mes propres jeux en utilisant des outils comme Unity. Je me concentre sur la réalisation de projets personnels qui me permettent d'explorer ma créativité et mes compétences techniques. ",
+		'Compétences Techniques' : 'Compétences Techniques',
+		"Modélisation d'objets 3D sur Blender" : "Modélisation d'objets 3D sur Blender",
+		"Passionné par la création d'objets 3D, je pratique régulièrement sur Blender pour perfectionner mes compétences en modélisation, texturisation et rendu. Mon expérience inclut la réalisation de modèles complexes et la maîtrise des outils avancés du logiciel." : "Passionné par la création d'objets 3D, je pratique régulièrement sur Blender pour perfectionner mes compétences en modélisation, texturisation et rendu. Mon expérience inclut la réalisation de modèles complexes et la maîtrise des outils avancés du logiciel.",
+		'Création de sites Internet' : 'Création de sites Internet',
+		"En apprentissage actif dans le domaine du développement web, je me forme à la création de sites internet en explorant diverses technologies et outils. Je mets en pratique mes connaissances en HTML, CSS et JavaScript pour concevoir et développer des sites web fonctionnels et esthétiques." : "En apprentissage actif dans le domaine du développement web, je me forme à la création de sites internet en explorant diverses technologies et outils. Je mets en pratique mes connaissances en HTML, CSS et JavaScript pour concevoir et développer des sites web fonctionnels et esthétiques.",
+		"Dans ce platformer, vous incarnez Aria, une héroïne courageuse en quête des âmes perdues. Ces âmes, sous forme d'armes puissantes, sont dispersées à travers différents niveaux, chaque niveau abritant une arme unique. Votre mission est de toutes les rassembler pour affronter Vespera, le grand méchant qui menace le monde et introduit l'histoire du jeu." : "Dans ce platformer, vous incarnez Aria, une héroïne courageuse en quête des âmes perdues. Ces âmes, sous forme d'armes puissantes, sont dispersées à travers différents niveaux, chaque niveau abritant une arme unique. Votre mission est de toutes les rassembler pour affronter Vespera, le grand méchant qui menace le monde et introduit l'histoire du jeu.",
+		"Minishell est un projet de l'école 42 où j'ai recréé un shell simple. Le but était d'apprendre à gérer l'exécution de commandes, les processus, les signaux et les variables d'environnement. Ce projet m'a permis de renforcer mes compétences en programmation système et d'acquérir une meilleure compréhension des systèmes d'exploitation." : "Minishell est un projet de l'école 42 où j'ai recréé un shell simple. Le but était d'apprendre à gérer l'exécution de commandes, les processus, les signaux et les variables d'environnement. Ce projet m'a permis de renforcer mes compétences en programmation système et d'acquérir une meilleure compréhension des systèmes d'exploitation.",
+		"Cub3D est un projet de l'école 42 où j'ai recréé un moteur graphique 3D en utilisant la bibliothèque miniLibX. L'objectif était de comprendre le raycasting, de gérer les textures, le déplacement du joueur et les collisions. Ce projet m'a permis de renforcer mes compétences en programmation graphique et de mieux comprendre le fonctionnement des moteurs 3D." : "Cub3D est un projet de l'école 42 où j'ai recréé un moteur graphique 3D en utilisant la bibliothèque miniLibX. L'objectif était de comprendre le raycasting, de gérer les textures, le déplacement du joueur et les collisions. Ce projet m'a permis de renforcer mes compétences en programmation graphique et de mieux comprendre le fonctionnement des moteurs 3D.",
+		"So_long est un projet de l'école 42 où j'ai recréé un jeu de type 'Sokoban' en 2D avec miniLibX. Le but était d'apprendre à gérer un moteur de jeu 2D et les événements clavier/souris. J'ai implémenté le rendu 2D, le déplacement du joueur, et la gestion des ennemis et objets, renforçant ainsi mes compétences en développement de jeux vidéo." : "So_long est un projet de l'école 42 où j'ai recréé un jeu de type 'Sokoban' en 2D avec miniLibX. Le but était d'apprendre à gérer un moteur de jeu 2D et les événements clavier/souris. J'ai implémenté le rendu 2D, le déplacement du joueur, et la gestion des ennemis et objets, renforçant ainsi mes compétences en développement de jeux vidéo.",
+		"Étudiant passionné et motivé, je suis toujours prêt à collaborer et à échanger. Contactez-moi pour discuter de projets ou d'opportunités !" : "Étudiant passionné et motivé, je suis toujours prêt à collaborer et à échanger. Contactez-moi pour discuter de projets ou d'opportunités !",
     };
     
     const en = {
@@ -248,6 +259,17 @@ document.addEventListener("DOMContentLoaded", function() {
         'Etudiant a 42 lyon': 'Student at 42 Lyon',
         "En tant qu'étudiant à l'école 42, je me consacre à l'apprentissage continu en informatique. Je développe mes compétences en programmation et en résolution de problèmes techniques au sein de cette école innovante.": "As a student at 42 school, I am dedicated to continuous learning in computer science. I am developing my programming skills and technical problem-solving within this innovative school.",
         'développeur de jeux vidéo': 'Video game developer',
+		"En tant que passionné de jeux vidéo, je crée et développe mes propres jeux en utilisant des outils comme Unity. Je me concentre sur la réalisation de projets personnels qui me permettent d'explorer ma créativité et mes compétences techniques.": "As a video game enthusiast, I create and develop my own games using tools like Unity. I focus on personal projects that allow me to explore my creativity and technical skills.",
+		'Compétences Techniques' : 'Technical Skills',
+		"Modélisation d'objets 3D sur Blender" : "3D Object Modeling on Blender",
+		"Passionné par la création d'objets 3D, je pratique régulièrement sur Blender pour perfectionner mes compétences en modélisation, texturisation et rendu. Mon expérience inclut la réalisation de modèles complexes et la maîtrise des outils avancés du logiciel." : "Passionate about creating 3D objects, I regularly practice on Blender to improve my skills in modeling, texturing, and rendering. My experience includes creating complex models and mastering advanced software tools.",
+		'Création de sites Internet' : 'Web Development',
+		"En apprentissage actif dans le domaine du développement web, je me forme à la création de sites internet en explorant diverses technologies et outils. Je mets en pratique mes connaissances en HTML, CSS et JavaScript pour concevoir et développer des sites web fonctionnels et esthétiques." : "Actively learning in the field of web development, I am training in creating websites by exploring various technologies and tools. I put my knowledge into practice in HTML, CSS, and JavaScript to design and develop functional and aesthetic websites.",
+		"Dans ce platformer, vous incarnez Aria, une héroïne courageuse en quête des âmes perdues. Ces âmes, sous forme d'armes puissantes, sont dispersées à travers différents niveaux, chaque niveau abritant une arme unique. Votre mission est de toutes les rassembler pour affronter Vespera, le grand méchant qui menace le monde et introduit l'histoire du jeu." : "In this platformer, you play as Aria, a brave heroine in search of lost souls. These souls, in the form of powerful weapons, are scattered throughout different levels, each level housing a unique weapon. Your mission is to gather them all to face Vespera, the big bad who threatens the world and introduces the game's story.",
+		"Minishell est un projet de l'école 42 où j'ai recréé un shell simple. Le but était d'apprendre à gérer l'exécution de commandes, les processus, les signaux et les variables d'environnement. Ce projet m'a permis de renforcer mes compétences en programmation système et d'acquérir une meilleure compréhension des systèmes d'exploitation." : "Minishell is a project from 42 school where I recreated a simple shell. The goal was to learn how to manage command execution, processes, signals, and environment variables. This project allowed me to strengthen my system programming skills and gain a better understanding of operating systems.",
+		"Cub3D est un projet de l'école 42 où j'ai recréé un moteur graphique 3D en utilisant la bibliothèque miniLibX. L'objectif était de comprendre le raycasting, de gérer les textures, le déplacement du joueur et les collisions. Ce projet m'a permis de renforcer mes compétences en programmation graphique et de mieux comprendre le fonctionnement des moteurs 3D." : "Cub3D is a project from 42 school where I recreated a 3D graphics engine using the miniLibX library. The goal was to understand raycasting, manage textures, player movement, and collisions. This project allowed me to strengthen my graphics programming skills and better understand how 3D engines work.",
+		"So_long est un projet de l'école 42 où j'ai recréé un jeu de type 'Sokoban' en 2D avec miniLibX. Le but était d'apprendre à gérer un moteur de jeu 2D et les événements clavier/souris. J'ai implémenté le rendu 2D, le déplacement du joueur, et la gestion des ennemis et objets, renforçant ainsi mes compétences en développement de jeux vidéo." : "So_long is a project from 42 school where I recreated a 'Sokoban' type game in 2D with miniLibX. The goal was to learn how to manage a 2D game engine and keyboard/mouse events. I implemented 2D rendering, player movement, and enemy and object management, thus strengthening my video game development skills.",
+		"Étudiant passionné et motivé, je suis toujours prêt à collaborer et à échanger. Contactez-moi pour discuter de projets ou d'opportunités !" : "Passionate and motivated student, I am always ready to collaborate and exchange. Contact me to discuss projects or opportunities!",
     };
     
     const es = {
@@ -264,6 +286,17 @@ document.addEventListener("DOMContentLoaded", function() {
         'Etudiant a 42 lyon': 'Estudiante en 42 Lyon',
         "En tant qu'étudiant à l'école 42, je me consacre à l'apprentissage continu en informatique. Je développe mes compétences en programmation et en résolution de problèmes techniques au sein de cette école innovante.": "Como estudiante en la escuela 42, estoy dedicado al aprendizaje continuo en ciencias de la computación. Estoy desarrollando mis habilidades de programación y resolución de problemas técnicos dentro de esta escuela innovadora.",
         'développeur de jeux vidéo': 'Desarrollador de videojuegos',
+		"En tant que passionné de jeux vidéo, je crée et développe mes propres jeux en utilisant des outils comme Unity. Je me concentre sur la réalisation de projets personnels qui me permettent d'explorer ma créativité et mes compétences techniques.": "Como entusiasta de los videojuegos, creo y desarrollo mis propios juegos utilizando herramientas como Unity. Me enfoco en proyectos personales que me permiten explorar mi creatividad y habilidades técnicas.",
+		'Compétences Techniques' : 'Habilidades Técnicas',
+		"Modélisation d'objets 3D sur Blender" : "Modelado de Objetos 3D en Blender",
+		"Passionné par la création d'objets 3D, je pratique régulièrement sur Blender pour perfectionner mes compétences en modélisation, texturisation et rendu. Mon expérience inclut la réalisation de modèles complexes et la maîtrise des outils avancés du logiciel." : "Apasionado por la creación de objetos 3D, practico regularmente en Blender para mejorar mis habilidades en modelado, texturizado y renderizado. Mi experiencia incluye la creación de modelos complejos y el dominio de herramientas avanzadas del software.",
+		'Création de sites Internet' : 'Desarrollo Web',
+		"En apprentissage actif dans le domaine du développement web, je me forme à la création de sites internet en explorant diverses technologies et outils. Je mets en pratique mes connaissances en HTML, CSS et JavaScript pour concevoir et développer des sites web fonctionnels et esthétiques." : "Aprendiendo activamente en el campo del desarrollo web, me estoy formando en la creación de sitios web explorando diversas tecnologías y herramientas. Pongo en práctica mis conocimientos en HTML, CSS y JavaScript para diseñar y desarrollar sitios web funcionales y estéticos.",
+		"Dans ce platformer, vous incarnez Aria, une héroïne courageuse en quête des âmes perdues. Ces âmes, sous forme d'armes puissantes, sont dispersées à travers différents niveaux, chaque niveau abritant une arme unique. Votre mission est de toutes les rassembler pour affronter Vespera, le grand méchant qui menace le monde et introduit l'histoire du jeu." : "En este juego de plataformas, juegas como Aria, una valiente heroína en busca de almas perdidas. Estas almas, en forma de poderosas armas, están dispersas en diferentes niveles, cada nivel albergando un arma única. Tu misión es reunirlas todas para enfrentarte a Vespera, el gran malvado que amenaza al mundo e introduce la historia del juego.",
+		"Minishell est un projet de l'école 42 où j'ai recréé un shell simple. Le but était d'apprendre à gérer l'exécution de commandes, les processus, les signaux et les variables d'environnement. Ce projet m'a permis de renforcer mes compétences en programmation système et d'acquérir une meilleure compréhension des systèmes d'exploitation." : "Minishell es un proyecto de la escuela 42 donde recreé un shell simple. El objetivo era aprender a gestionar la ejecución de comandos, procesos, señales y variables de entorno. Este proyecto me permitió fortalecer mis habilidades de programación de sistemas y adquirir una mejor comprensión de los sistemas operativos.",
+		"Cub3D est un projet de l'école 42 où j'ai recréé un moteur graphique 3D en utilisant la bibliothèque miniLibX. L'objectif était de comprendre le raycasting, de gérer les textures, le déplacement du joueur et les collisions. Ce projet m'a permis de renforcer mes compétences en programmation graphique et de mieux comprendre le fonctionnement des moteurs 3D." : "Cub3D es un proyecto de la escuela 42 donde recreé un motor gráfico 3D utilizando la biblioteca miniLibX. El objetivo era comprender el raycasting, gestionar texturas, movimiento de jugadores y colisiones. Este proyecto me permitió fortalecer mis habilidades de programación gráfica y comprender mejor cómo funcionan los motores 3D.",
+		"So_long est un projet de l'école 42 où j'ai recréé un jeu de type 'Sokoban' en 2D avec miniLibX. Le but était d'apprendre à gérer un moteur de jeu 2D et les événements clavier/souris. J'ai implémenté le rendu 2D, le déplacement du joueur, et la gestion des ennemis et objets, renforçant ainsi mes compétences en développement de jeux vidéo." : "So_long es un proyecto de la escuela 42 donde recreé un juego de tipo 'Sokoban' en 2D con miniLibX. El objetivo era aprender a gestionar un motor de juego 2D y eventos de teclado/ratón. Implementé el renderizado 2D, el movimiento del jugador y la gestión de enemigos y objetos, fortaleciendo así mis habilidades en el desarrollo de videojuegos.",
+		"Étudiant passionné et motivé, je suis toujours prêt à collaborer et à échanger. Contactez-moi pour discuter de projets ou d'opportunités !" : "Estudiante apasionado y motivado, siempre estoy listo para colaborar e intercambiar ideas. ¡Contáctame para discutir proyectos u oportunidades!",
     };
     
     // Fonction pour mettre à jour les éléments avec data-translate
@@ -290,25 +323,24 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     
     // Gestion des événements pour changer la langue
-    langItems.forEach(item => {
-        item.addEventListener('click', function() {
-            let language;
-            if (item.innerText === 'Français') {
-                language = 'fr';
-            } else if (item.innerText === 'Anglais') {
-                language = 'en';
-            } else if (item.innerText === 'Español') {
-                language = 'es';
-            }
-            langText.innerText = item.innerText; // Met à jour le texte du bouton
-            langList.style.display = 'none'; // Cache la liste
-            changeLanguage(language); // Applique la langue sélectionnée
-        });
-    });
-
-
-
-
-
-
+	document.querySelectorAll('.lang-item').forEach(item => {
+		item.addEventListener('click', function() {
+			let language;
+			const cleanText = item.innerText.split(' ')[0].trim();
+	
+			if (cleanText === 'Français') {
+				language = 'fr';
+			} else if (cleanText === 'Anglais') {
+				language = 'en';
+			} else if (cleanText === 'Español') {
+				language = 'es';
+			}
+	
+			const langText = document.getElementById('lang-text');
+			const langList = document.getElementById('lang-list');
+			langText.innerText = item.innerText; // Met à jour le texte du bouton avec emoji inclus
+			langList.style.display = 'none'; // Cache la liste
+			changeLanguage(language); // Applique la langue sélectionnée
+		});
+	});
 });
