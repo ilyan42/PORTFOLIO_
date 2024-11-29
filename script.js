4bb41fc0518d0ea8528f7d2d46b97de4487cd35a
ilyan <ilyanbendib@gmail.com>
2024-11-29 21:54:15 +0100

    ajout container unreal

--- script.js
+++ script.js
@@ -343,4 +343,39 @@ document.addEventListener("DOMContentLoaded", function() {
 			changeLanguage(language); // Applique la langue sélectionnée
 		});
 	});
+
+
+	const leftZones = document.getElementsByClassName('left-zone');
+	const rightZones = document.getElementsByClassName('right-zone');
+	const premierJeux = document.getElementsByClassName('premierJeux');
+	const ImagePremierJeux = document.getElementById('ImagePremierJeux');
+	const LinkPremierJeux = document.getElementById('LinkPremierJeux');
+	const containerUnreal = document.querySelector('.container-unreal');
+	
+	// Ajouter un événement au bouton
+	const firstGameButton = document.getElementsByClassName('zone-button1')[0];  // Prendre le premier élément de la collection
+	
+	if (firstGameButton) {
+		firstGameButton.addEventListener('click', function() {
+			// Masquer la zone gauche et droite
+			if (leftZones.length > 0) {
+				leftZones[0].style.display = 'none';  // Masquer le premier élément de la collection
+			}
+			if (rightZones.length > 0) {
+				rightZones[0].style.display = 'none';  // Masquer le premier élément de la collection
+			}
+			if (premierJeux.length > 0) {
+				premierJeux[0].style.display = 'flex';  // Afficher le premier élément de la collection
+			}
+			if (ImagePremierJeux) {
+				ImagePremierJeux.style.display = 'flex';  // Afficher l'image
+			}
+			if (LinkPremierJeux) {
+				LinkPremierJeux.style.display = 'flex';  // Afficher le lien
+			}
+			if (containerUnreal) {
+				containerUnreal.style.background = 'linear-gradient(135deg, #7e0000, #000000)';
+			}
+		});
+	}
 });
